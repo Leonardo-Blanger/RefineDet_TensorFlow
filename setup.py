036a6fec8f12e632d9b5c025d783e0b1a03f1b93
Leonardo Blanger <leonardoblanger@gmail.com>
2021-05-23 16:40:27 -0300

    Setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='refinedet_tensorflow',
+    packages=find_packages(include=['refinedet_tensorflow']),
+    version='0.1.0',
+    description='RefineDet Object Detection architecture in Tensorflow',
+    author='Leonardo Blanger',
+)
